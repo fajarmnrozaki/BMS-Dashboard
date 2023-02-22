e73cb84d14afe050d8c13bf34e1e42e9b1d3299f
fajarmnrozaki <zakifajar96@gmail.com>
2023-02-23 00:58:18 +0700

    configuration for the local

--- database/config.php
+++ database/config.php
@@ -1,13 +1,13 @@
 <?php
-// $host = 'localhost';
-// $user = 'root';
-// $pass = '';
-// $database = 'monitoring';
-
-$host = '192.168.18.19';
-$user = 'itbdelabo';
-$pass = 'delabo0220';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
 $database = 'monitoring';
+
+// $host = '192.168.18.19';
+// $user = 'itbdelabo';
+// $pass = 'delabo0220';
+// $database = 'monitoring';
 
 $connect = mysqli_connect($host, $user, $pass, $database);
 
